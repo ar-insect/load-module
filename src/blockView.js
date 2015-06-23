ed3e28049c920e08756d6b587c72934a5c738035
ar-insect <ar.insect@gmail.com>
2015-06-23 11:54:56 +0800

    update

--- src/blockView.js
+++ src/blockView.js
@@ -6,17 +6,17 @@
     events: {},
     className: 'sp-block',
     initialize: function () {
-
+      // 模板引擎预处理
       this.template = _.isString(this.template) ?
                     _.template(this.template) : this.template; // 初始化模板
 
-      //this.trigger('data:launch');
+      this.trigger('data:launch');
     },
     render: function () {
       this.template && alert( this.model.get('rows') );
     },
     getWidget: function(name) {
-
+      // 由block.getWidget代理
     }
   });
 
