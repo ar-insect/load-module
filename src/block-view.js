a1f01d9bbf64961f97208f37f8282617a19aa0e1
阿虫 <ar.insect@gmail.com>
2015-06-30 17:34:32 +0800

    Update block-view.js

--- src/block-view.js
+++ src/block-view.js
@@ -1,29 +1,27 @@
 var _ = require('lodash');
-var Backbone = require('backbone');
+  var Backbone = require('backbone');
 
-var BlockView = Backbone.View.extend({
-  events: {},
-  className: 'sp-block',
-  initialize: function () {
-    //预编译模板
-    var handlebars = this.getWidget('handlebars');
-    this.template = _.isString(this.template) ? (handlebars && handlebars.compile(this.template)) : null;
-    this.trigger('block:start', this.$el);
-  },
-  render: function () {
-    var html, node;
-    var data = this.model.get('data');
-    if (this.template) {
-      html = this.template(data);
-      node = $(this.el).empty().append(html);
-    } else {
-      node = data;
+  var BlockView = Backbone.View.extend({
+    events: {},
+    className: 'sp-block',
+    initialize: function () {
+      //预编译模板
+      var handlebars = this.getWidget('handlebars');
+      this.template = _.isString(this.template) ? (handlebars && handlebars.compile(this.template)) : null;
+      this.trigger('block:start', this.$el);
+    },
+    render: function () {
+      var html, node;
+      var data = this.model.get('data');
+      if (this.template) {
+        html = this.template(data);
+        node = $(this.el).empty().append(html);
+      } else {
+        node = data;
+      }
+      this.model.set('node', node);
+    },
+    getWidget: function(name) {
+      // 由block.getWidget代理
     }
-    this.trigger('whole:render', node);
-  },
-  getWidget: function (name) {
-    // 由block.getWidget代理
-  }
-});
-
-module.exports = BlockView;
+  });
