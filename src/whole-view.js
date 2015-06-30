71c888cf56ce894f3029f693b3436952edf13115
阿虫 <ar.insect@gmail.com>
2015-06-30 17:36:47 +0800

    Update whole-view.js

--- src/whole-view.js
+++ src/whole-view.js
@@ -1,22 +1,23 @@
-var _ = require('lodash');
-var Backbone = require('backbone');
-// widget
-var widget = {
-  handlebars: require('handlebars')
-};
+  var _ = require('lodash');
+  var Backbone = require('backbone');
+  // widget
+  var widget = {
+    handlebars: require('handlebars')
+  };
 
-var WholeView = Backbone.View.extend({
-  className: 'sp-wrap',
-  el: $('.container'),
-  initialize: function () {
-    //this.constructor.prototype.widget = widget;
-  },
-  render: function (node) {
-    $(this.el).append(node);
-  },
-  getWidget: function (name) {
-    return widget[name];
-  }
-});
+  var WholeView = Backbone.View.extend({
+    className: 'sp-wrap',
+    el: $('.container'),
+    initialize: function () {
+      //this.constructor.prototype.widget = widget;
+    },
+    render: function() {
+      var last = this.model.changed; // 自最后一组已改变的模型
+      $(this.el).append( _.values(last)[0] );
+    },
+    getWidget: function(name) {
+      return widget[name];
+    }
+  });
 
-module.exports = WholeView;
+  module.exports = WholeView;
