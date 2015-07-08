bd59739d812303111d3b40a07fabf40d5be08935
ar-insect <ar.insect@gmail.com>
2015-07-08 15:35:48 +0800

    add loading

--- src/block.js
+++ src/block.js
@@ -1,46 +1,54 @@
 var _ = require('lodash');
-  var Backbone = require('backbone');
+var Backbone = require('backbone');
 
-  var Block = Backbone.Model.extend({
-    initialize: function () {
-      //console.log( this );
-    },
-    validate: function (attributes, options) {
-      // validate 验证
-      //if (_.trim(this.get('value')) === '') {
-      //    return '{title}不能为空！';
-      // }
-      //alert('这里触发验证逻辑！');
-    },
-    start: function(el) {
-      // 向服务器发送请求
-      var _self = this;
-      var url = this.get('url');
-      var param = this.get('param');
-      // loading...
-      $('<span>正在加载中...</span>').appendTo(el);
-      if (url.indexOf('.json') > -1) {
-        this.save(param, {
-          url: url,
-          wait: true, // 等待服务器端设置新的属性
-          success: function (model, resp, options) {
-            _self.trigger('whole:set', _self, resp);
-          },
-          error: function (model, resp, options) {
-            //this.doError
-            //console.log(resp);
-          }
-        });
-      } else {
-        $(el).load(url, param, function() {
-          var data = arguments[0];
-          var result = arguments[1];
-          if ('success' === result) {
-            _self.trigger('whole:set', _self, el);
-          }
-        });
-      }
+var Block = Backbone.Model.extend({
+  initialize: function () {
+    //console.log( this );
+  },
+  loadingNode: undefined,
+  validate: function (attributes, options) {
+    // validate 验证
+    //if (_.trim(this.get('value')) === '') {
+    //    return '{title}不能为空！';
+    // }
+    //alert('这里触发验证逻辑！');
+  },
+  loading: function (cls, parent) {
+    //console.log(arguments);
+    this.loadingNode = $('<div class="' + cls + '"><span>正在加载中...</span></div>').appendTo(parent);
+  },
+  unloading: function (node) {
+    $(node).remove();
+  },
+  start: function (el) {
+    // 向服务器发送请求
+    var _self = this;
+    var url = this.get('url');
+    var param = this.get('param');
+    if (url.indexOf('.json') > -1) {
+      this.save(param, {
+        url: url,
+        wait: true, // 等待服务器端设置新的属性
+        success: function (model, resp, options) {
+          _self.trigger('block:unloading', _self.loadingNode);
+          _self.trigger('whole:set', _self, resp);
+        },
+        error: function (model, resp, options) {
+          //this.doError
+          //console.log(resp);
+        }
+      });
+    } else {
+      $(el).load(url, param, function () {
+        var data = arguments[0];
+        var result = arguments[1];
+        if ('success' === result) {
+          _self.trigger('block:unloading', _self.loadingNode);
+          _self.trigger('whole:set', _self, el);
+        }
+      });
     }
-  });
+  }
+});
 
-  module.exports = Block;
+module.exports = Block;
--- src/whole.js
+++ src/whole.js
@@ -1,20 +1,20 @@
-  var _ = require('lodash');
-  var Backbone = require('backbone');
+var _ = require('lodash');
+var Backbone = require('backbone');
 
-  var Whole = Backbone.Model.extend({
-    initialize: function () {
-      // console.log(this.coll);
-    },
-    setData: function() {
-      var model = arguments[0];
-      var data = arguments[1];
-      var cid = model.cid;
-      var the = this.collection.get(cid);
-      if (cid === the.cid) {
-        the.set('data', data);
-        this.set(cid, the.get('node'));
-      }
+var Whole = Backbone.Model.extend({
+  initialize: function () {
+    // console.log(this.coll);
+  },
+  setData: function () {
+    var model = arguments[0];
+    var data = arguments[1];
+    var cid = model.cid;
+    var the = this.collection.get(cid);
+    if (cid === the.cid) {
+      the.set('data', data);
+      this.set(cid, the.get('node'));
     }
-  });
+  }
+});
 
-  module.exports = Whole;
+module.exports = Whole;
